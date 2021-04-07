83f3f558ddc73fdd08f537148ce4c481bcfd3701
Gaurav Prasad <prasadgaurav559@gmail.com>
2021-04-07 22:53:12 +0530

    Update response.js

--- response.js
+++ response.js
@@ -14,6 +14,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.get('/users',(req,res)=>{
     res.json({
